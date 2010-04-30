4f15d0d4e86725f224583d8afc8d4539cc6ca0ac
yuki <yuki@dodo>
2010-05-01 06:31:25 +0900

    Modify Setting activity Show current value in summary.

--- src/org/hogeika/android/Sharup/Setting.java
+++ src/org/hogeika/android/Sharup/Setting.java
@@ -1,16 +1,47 @@
 package org.hogeika.android.Sharup;
 
-import org.hogeika.android.Sharup.R;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
 
-public class Setting extends PreferenceActivity{
+public class Setting extends PreferenceActivity implements OnPreferenceChangeListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.pref);
+		
+		PreferenceScreen ps = getPreferenceScreen();
+		int count = ps.getPreferenceCount();
+		for(int i = 0; i < count; i++){
+			Preference pref = ps.getPreference(i);
+			if(pref instanceof EditTextPreference){
+				EditTextPreference edit = (EditTextPreference)pref;
+				edit.setSummary(edit.getText());
+			}
+			if(pref instanceof ListPreference){
+				ListPreference list = (ListPreference)pref;
+				list.setSummary(list.getEntry());
+			}
+			pref.setOnPreferenceChangeListener(this);
+		}
+	}
+
+	public boolean onPreferenceChange(Preference pref, Object value) {
+		if(pref instanceof EditTextPreference){
+			EditTextPreference edit = (EditTextPreference)pref;
+			edit.setSummary((String)value);
+		}
+		if(pref instanceof ListPreference){
+			ListPreference list = (ListPreference)pref;
+			list.setSummary(list.getEntries()[list.findIndexOfValue((String)value)]);
+		}
+		return true;
 	}
 	
 
